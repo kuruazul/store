ab3d2d121369b48f73a271d28e71b97f8372c558
kuruazul <ejcb200@hotmail.com>
2021-09-13 00:12:50 -0500

    adding template base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+//  importing sections
+import Nav from './components/Nav'
+import Footer from './components/Footer'
+import Home from './components/home/Index'
 import './App.css';
+import './styles.css';
+import './scripts.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Home/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/home/Index.js
@@ -0,0 +1,15 @@
+import AboutUs from "./AboutUs"
+import Categories from "./Categories"
+import Image from "./Image"
+ import { Fragment } from "react"
+const Index = () => {
+    return(
+    <Fragment>
+        <Image/> ,
+        <Categories/>,
+        <AboutUs/>
+    </Fragment>
+    )
+}
+
+export default Index
