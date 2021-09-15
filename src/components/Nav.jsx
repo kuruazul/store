6f9b061b6691690387c77cc882f61d82f783d754
Erick calzada <ejcb200@hotmail.com>
2021-09-14 19:40:05 -0500

    adding api fetch

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,20 +1,43 @@
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Nav = () => {
     return(
-        <nav class="navbar navbar-expand-lg  mx-auto bg-secondary text-uppercase fixed-top" id="mainNav">
-            <div class="container">
-                <a class="navbar-brand" href="#page-top">Start Bootstrap</a>
-                <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    Menu
-                    <i class="fas fa-bars"></i>
+        <nav className="navbar navbar-expand-lg  mx-auto bg-secondary text-uppercase fixed-top" id="mainNav">
+            <div className="container">
+                <a className="navbar-brand" href="#page-top">Maopesa</a>
+                <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu 
+                    <i className="fas fa-bars"></i>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ms-auto">
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#portfolio">Portfolio</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
-                        <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="#contact">Contact</a></li>
+                <div className="collapse navbar-collapse" id="navbarResponsive">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item mx-0 mx-lg-1" data-bs-toggle="modal" data-bs-target="#shoppingCart"><a className="nav-link py-3 px-0 px-lg-3 btn rounded"><FontAwesomeIcon icon={faShoppingCart}/> </a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#categories">Categorias</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#about">About</a></li>
+                        <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded" href="#contact"></a></li>
                     </ul>
                 </div>
             </div>
+            
+            <div className="modal fade" id="shoppingCart">
+                <div className="modal-dialog" role="document">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div className="modal-body">
+                        ...
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Save changes</button>
+                    </div>
+                    </div>
+                </div>
+                </div>
         </nav>
     )
 }
--- src/components/home/Image.jsx
+++ src/components/home/Image.jsx
@@ -1,11 +1,13 @@
+
 const Image = () => {
     return(
-        <header class="masthead bg-primary text-white text-center">
-            <div class="container d-flex align-items-center flex-column">
+        <header class="masthead bg-primary text-white text-center position-relative">
+            
+            <div class="container d-flex align-items-center flex-column position-absolute">
                 
-                <img class="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="..." />
                 
-                <h1 class="masthead-heading text-uppercase mb-0">Start Bootstrap</h1>
+                
+                <h1 class="masthead-heading text-uppercase mb-0">MAOPESA </h1>
                 
                 <div class="divider-custom divider-light">
                     <div class="divider-custom-line"></div>
@@ -13,7 +15,7 @@ const Image = () => {
                     <div class="divider-custom-line"></div>
                 </div>
                 
-                <p class="masthead-subheading font-weight-light mb-0">Graphic Artist - Web Designer - Illustrator</p>
+                <p class="masthead-subheading font-weight-light mb-0">Ferreterìa - Agregados - Conèxiones</p>
             </div>
         </header>
     )
